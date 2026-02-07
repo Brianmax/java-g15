f819e64717c5b99ec7d77fb341b91f0469dcf656
george <george.maxi@ucsp.edu.pe>
2026-02-06 22:21:57 -0500

    poo intro

---
+++ poo/src/Main.java
@@ -0,0 +1,15 @@
+import pooTeoria.Usuario;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        Usuario usuario1 = new Usuario();
+        Usuario usuario2 = new Usuario();
+
+        usuario1.username = "george123";
+        usuario1.edad = 29;
+
+        usuario2.username = "fiore12";
+        usuario2.edad = 30;
+    }
+}
---
+++ poo/src/pooTeoria/Usuario.java
@@ -0,0 +1,10 @@
+package pooTeoria;
+
+public class Usuario {
+    // atributos
+    public String username;
+    public String email;
+    public int edad;
+    public String sexo;
+    public String pais;
+}

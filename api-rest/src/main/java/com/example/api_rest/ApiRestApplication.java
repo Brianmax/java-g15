3bab01edf9767bb9e18f4d1435454e1a72d56aa2
george <george.maxi@ucsp.edu.pe>
2026-03-18 21:59:32 -0500

    pom fix

--- api-rest/src/main/java/com/example/api_rest/ApiRestApplication.java
+++ api-rest/src/main/java/com/example/api_rest/ApiRestApplication.java
@@ -3,10 +3,12 @@ package com.example.api_rest;
 import com.example.api_rest.entity.ArticuloEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
